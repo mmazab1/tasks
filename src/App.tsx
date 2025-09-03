bd3c01bcf855842764b067958204e46b12b6da7a
Lab0 <msmazabras@loyola.edu>
2025-09-03 18:28:25 -0400

    hello world addition

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): React.JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Matthew Mazabras.
+                automatically reload. Matthew Mazabras. Hello World
             </p>
         </div>
     );
