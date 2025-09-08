c524e75acd6650aa20a854d9c70169610659fea8
Lab0 <msmazabras@loyola.edu>
2025-09-07 20:13:50 -0400

    solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import "./App.css";
 
 function App(): React.JSX.Element {
@@ -7,10 +9,50 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <img src="C:/Users/matth/tasks/src/photo.png" alt="Picture" />
+            <h1>First Header</h1>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Matthew Mazabras. Hello World
+                <Button
+                    onClick={() => {
+                        console.log("Hello World!");
+                    }}
+                >
+                    Log Hello World
+                </Button>
             </p>
+            <ul>
+                <li>First thing</li>
+                <li>Another thing</li>
+                <li>A third item</li>
+            </ul>
+            <Container>
+                <Row>
+                    <Col>
+                        First column.
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "50px",
+                                backgroundColor: "red",
+                                marginTop: "10px",
+                            }}
+                        />
+                    </Col>
+                    <Col>
+                        Second column.
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "50px",
+                                backgroundColor: "red",
+                                marginTop: "10px",
+                            }}
+                        />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
