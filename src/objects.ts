3c9b53a713e53aa66e219b3d6bbfb4451afe63ae
Lab0 <msmazabras@loyola.edu>
2025-09-19 15:47:52 -0400

    done

--- src/objects.ts
+++ src/objects.ts
@@ -44,10 +44,8 @@ export function isCorrect(question: Question, answer: string): boolean {
 export function isValid(question: Question, answer: string): boolean {
     if (question.type === "short_answer_question") {
         return true;
-    } else if (question.type === "multiple_choice_question") {
-        return question.options.includes(answer);
     }
-    return false;
+    return question.options.includes(answer);
 }
 
 /**
