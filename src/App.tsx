550a1f90d581fae6012c581ac7b5b04653e0db44
Lab0 <msmazabras@loyola.edu>
2025-09-30 18:33:53 -0400

    check

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Button } from "react-bootstrap";
-import { Container, Row, Col } from "react-bootstrap";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
